ffe6fac5cc5ae13c6654901b819d8131317ee448
shokhaway <shoxauey@gmail.com>
2022-07-02 15:49:27 +0300

    newwww

---
+++ clc-main/script.js
@@ -0,0 +1,12 @@
+function numCatch(a){
+    let inp=document.getElementById('input1');
+    inp.value=inp.value+a.value;
+}
+function barobar(){
+    let r=document.getElementById('input1');
+    r.value=eval(r.value);
+}
+function reset(){
+    let res=document.getElementById('input1');
+    res.value="";
+}
